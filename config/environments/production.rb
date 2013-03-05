644d4b81cfabfc2ecb0f130f4f000512d024d010
jdd <jdd@netposa.com>
2013-03-05 14:35:50 +0800

    rm *.scss & js.coffee & production.rb add assets file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Woji::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( application.js bootstrap.min.js )
+   config.assets.precompile += %w(application.css bootstrap.min.css main.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
