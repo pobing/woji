06aca6b73c517b1a2fed168335003e84ccd4dd67
jdd <jdd@netposa.com>
2013-05-23 17:08:04 +0800

    update flick.rb

--- app/models/flick.rb
+++ app/models/flick.rb
@@ -1,13 +1,13 @@
-require 'flickraw'
+#require 'flickraw'
 
-FlickRaw.api_key="112874de58604e65d3c74f8593191a0c"
-FlickRaw.shared_secret="df1fcbc8c1810a54"
+#FlickRaw.api_key="112874de58604e65d3c74f8593191a0c"
+#FlickRaw.shared_secret="df1fcbc8c1810a54"
 
-list   = flickr.photos.getContactsPhotos(:include_self => 'include_self')
+#list   = flickr.photos.getContactsPhotos(:include_self => 'include_self')
 #getContactsPhotos(:count => 'count',:just_friends => 'just_friends',:single_photo => 'single_photo',:include_self => 'include_self',:extras => 'extras')
 # list = flickr.photos
 # list = flickr.photos.getContactsPublicPhotos
-puts list[0].inspect
+#puts list[0].inspect
 # id     = list[0].id
 # secret = list[0].secret
 # info = flickr.photos.getInfo :photo_id => id, :secret => secret
