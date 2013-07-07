f375c5cf5e55c0c3dc5ed47bbcaacc7d6a3c9e7d
pobing <cn.jdong@gmail.com>
2013-07-07 21:18:03 +0800

    fix PG::Error: ERROR

--- db/migrate/20130707063841_change_uid_type_from_authentication.rb
+++ db/migrate/20130707063841_change_uid_type_from_authentication.rb
@@ -1,8 +1,14 @@
 class ChangeUidTypeFromAuthentication < ActiveRecord::Migration
-  def up
-  	change_column :authentications,:uid,:bigint # for pg
-  end
+  # def up
+  # 	change_column :authentications,:uid,:bigint # for pg
+  # end
 
+ def up
+    execute <<-SQL
+      ALTER TABLE authentications  ALTER COLUMN uid TYPE bigint USING uid::bigint
+    SQL
+  end
   def down
+    change_column :users, :uid, :string
   end
 end
